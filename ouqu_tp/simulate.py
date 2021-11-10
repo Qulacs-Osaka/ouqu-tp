e00892fc5ca0afc6c003b92ed5cb3d164232cd86
Wataru Inoue <wattan1729@gmail.com>
2021-11-10 20:12:38 +0900

    shot できた

--- ouqu_tp/simulate.py
+++ ouqu_tp/simulate.py
@@ -2,9 +2,24 @@ from qulacs import QuantumCircuit, QuantumState
 from qulacs.state import inner_product
 
 
-def simulate_circuit(cir: QuantumCircuit) -> QuantumState:
+from ot_io import input_strings, output_gates_QASMfuu, str_to_gate
+from tran import tran_ouqu_multi
 
-    n_qubit=cir.get_qubit_count()
-    sta=QuantumState(n_qubit)
-    cir.update_quantum_state(sta)
-    return sta
+
+input_strs = input_strings()
+(n_qubit, input_list) = str_to_gate(input_strs)
+
+#input_listを直接ぶち込む
+testcircuit = QuantumCircuit(n_qubit)
+for it in input_list:
+    testcircuit.add_gate(it)
+out_state=QuantumState(n_qubit)
+testcircuit.update_quantum_state(out_state)
+shots=100
+kekka = out_state.sampling(shots)
+#print(kekka)
+for aaaa in kekka:
+    moziretu='{:b}'.format(aaaa)
+    while len(moziretu)<n_qubit:
+        moziretu="0"+moziretu
+    print(moziretu)
