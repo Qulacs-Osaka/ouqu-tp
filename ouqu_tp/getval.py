0d2ed787ba5154ce64e6baaca0b33f06b49aa860
Wataru Inoue <wattan1729@gmail.com>
2021-12-17 19:10:08 +0900

    a

--- ouqu_tp/getval.py
+++ ouqu_tp/getval.py
@@ -1,7 +1,7 @@
 import sys
 
 from ot_io import input_strings, str_to_gate
-from qulacs import QuantumCircuit, QuantumState,observable
+from qulacs import QuantumCircuit, QuantumState, observable
 
 input_strs = input_strings()
 (n_qubit, input_list) = str_to_gate(input_strs, "notput")
@@ -14,7 +14,6 @@ for it in input_list:
 out_state = QuantumState(n_qubit)
 testcircuit.update_quantum_state(out_state)
 
-obs=observable.create_observable_from_openfermion_file(sys.argv[1])
+obs = observable.create_observable_from_openfermion_file(sys.argv[1])
 
 print(obs.get_expectation_value(out_state))
-
