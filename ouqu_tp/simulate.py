67942972828283dbd5a63b2f4a926607cb6b221e
Wataru Inoue <wattan1729@gmail.com>
2021-12-22 19:33:08 +0900

    test書いた

--- ouqu_tp/simulate.py
+++ ouqu_tp/simulate.py
@@ -3,21 +3,24 @@ import sys
 from ot_io import input_strings, str_to_gate
 from qulacs import QuantumCircuit, QuantumState
 
+def simulate_do(input_strs ,shots:int):
+    (n_qubit, input_list) = str_to_gate(input_strs, "notput")
+    testcircuit = QuantumCircuit(n_qubit)
+    for it in input_list:
+        testcircuit.add_gate(it)
+    out_state = QuantumState(n_qubit)
+    testcircuit.update_quantum_state(out_state)
+    kekka = out_state.sampling(shots)
+    return kekka
+    
+
 input_strs = input_strings()
-(n_qubit, input_list) = str_to_gate(input_strs, "notput")
-
-# input_listを直接ぶち込む
-
-testcircuit = QuantumCircuit(n_qubit)
-for it in input_list:
-    testcircuit.add_gate(it)
-out_state = QuantumState(n_qubit)
-testcircuit.update_quantum_state(out_state)
 shots = int(sys.argv[1])
-kekka = out_state.sampling(shots)
-# print(kekka)
+kekka=simulate_do(input_strs,shots)
+# input_listを直接ぶち込む
 for aaaa in kekka:
     moziretu = "{:b}".format(aaaa)
     while len(moziretu) < n_qubit:
         moziretu = "0" + moziretu
     print(moziretu)
+    
