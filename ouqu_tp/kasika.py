1749ba5cee31c21edc8b645770f4610bf29f89ce
Wataru Inoue <wattan1729@gmail.com>
2022-03-04 16:20:44 +0900

    qasm_fix

---
+++ ouqu_tp/kasika.py
@@ -0,0 +1,9 @@
+
+from qiskit import *
+import matplotlib.pyplot as plt
+import numpy as np
+
+input_circuit =QuantumCircuit.from_qasm_file('sample/input.qasm')
+input_circuit.draw(output='mpl').savefig('sample/input_qasm_graph.png')
+output_circuit =QuantumCircuit.from_qasm_file('sample/output.qasm')
+output_circuit.draw(output='mpl').savefig('sample/output_qasm_graph.png')
